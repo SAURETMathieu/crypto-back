a96346c92ebc4046bba9958711ef3883e3462f40
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-04 19:21:22 +0200

    feat(prisma): add migration with all the tables we need

---
+++ prisma/migrations/20240604123709_init/migration.sql
@@ -0,0 +1,317 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "WalletType" AS ENUM ('Centralized', 'Decentralized');
+
+-- CreateEnum
+CREATE TYPE "WalletExchange" AS ENUM ('Binance', 'Coinbase', 'Kraken', 'Bitstamp', 'Kucoin');
+
+-- CreateEnum
+CREATE TYPE "WalletBlockchain" AS ENUM ('All', 'Bitcoin', 'Ethereum', 'BSC', 'Polygon', 'Solana', 'Avalanche');
+
+-- CreateEnum
+CREATE TYPE "SubscriptionStatus" AS ENUM ('ACTIVE', 'CANCELED', 'PAST_DUE', 'TRIAL');
+
+-- CreateEnum
+CREATE TYPE "InvoiceStatus" AS ENUM ('PENDING', 'PAID', 'FAILED', 'CANCELED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("provider","providerAccountId")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier","token")
+);
+
+-- CreateTable
+CREATE TABLE "Wallet" (
+    "id" SERIAL NOT NULL,
+    "address" TEXT,
+    "blockchain" "WalletBlockchain" NOT NULL DEFAULT 'All',
+    "exchange" "WalletExchange",
+    "name" TEXT NOT NULL,
+    "type" "WalletType" NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Wallet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Crypto" (
+    "id" SERIAL NOT NULL,
+    "asset" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "digit" INTEGER NOT NULL,
+    "logo_url" TEXT NOT NULL,
+    "currency" TEXT NOT NULL DEFAULT 'USDT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Crypto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WalletCryptos" (
+    "walletId" INTEGER NOT NULL,
+    "cryptoId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WalletCryptos_pkey" PRIMARY KEY ("walletId","cryptoId")
+);
+
+-- CreateTable
+CREATE TABLE "Trade" (
+    "id" SERIAL NOT NULL,
+    "buy_price" DOUBLE PRECISION NOT NULL,
+    "sell_price" DOUBLE PRECISION NOT NULL,
+    "buy_quantity" DOUBLE PRECISION NOT NULL,
+    "sell_quantity" DOUBLE PRECISION NOT NULL,
+    "buy_timestamp" TIMESTAMP(3) NOT NULL,
+    "sell_timestamp" TIMESTAMP(3) NOT NULL,
+    "status" TEXT NOT NULL,
+    "buy_fees" DOUBLE PRECISION NOT NULL,
+    "sell_fees" DOUBLE PRECISION NOT NULL,
+    "currency" TEXT NOT NULL,
+    "cryptoId" INTEGER NOT NULL,
+    "botId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Trade_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ApiKey" (
+    "id" SERIAL NOT NULL,
+    "key" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "write" BOOLEAN NOT NULL,
+    "read" BOOLEAN NOT NULL,
+    "expired_at" TIMESTAMP(3) NOT NULL,
+    "walletId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ApiKey_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bot" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "starting_balance" DOUBLE PRECISION NOT NULL,
+    "ending_balance" DOUBLE PRECISION NOT NULL,
+    "max_trade_open" INTEGER NOT NULL,
+    "max_invest" DOUBLE PRECISION NOT NULL,
+    "apiKeyId" INTEGER NOT NULL,
+    "walletId" INTEGER NOT NULL,
+    "cryptoId" INTEGER NOT NULL,
+    "strategieId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Bot_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Strategy" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "variation" TEXT NOT NULL,
+    "execution_delay" INTEGER NOT NULL DEFAULT 60,
+    "percent_per_trade" DOUBLE PRECISION NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Strategy_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Plan" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "currency" TEXT NOT NULL,
+    "interval" TEXT NOT NULL,
+    "intervalCount" INTEGER NOT NULL,
+    "stripeId" TEXT NOT NULL,
+    "trialPeriodDays" INTEGER NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "features" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Plan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "planId" INTEGER NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMP(3),
+    "status" "SubscriptionStatus" NOT NULL,
+    "currentPeriodEnd" TIMESTAMP(3),
+    "lastPaymentDate" TIMESTAMP(3),
+    "nextPaymentDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "subscriptionId" INTEGER NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "currency" TEXT NOT NULL,
+    "status" "InvoiceStatus" NOT NULL,
+    "issuedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "paidAt" TIMESTAMP(3),
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "stripeInvoiceId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Wallet_userId_name_address_key" ON "Wallet"("userId", "name", "address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Crypto_asset_key" ON "Crypto"("asset");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Crypto_name_key" ON "Crypto"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ApiKey_key_walletId_key" ON "ApiKey"("key", "walletId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Bot_name_walletId_key" ON "Bot"("name", "walletId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Bot_cryptoId_walletId_strategieId_key" ON "Bot"("cryptoId", "walletId", "strategieId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Strategy_name_key" ON "Strategy"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Strategy_variation_key" ON "Strategy"("variation");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Plan_name_key" ON "Plan"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_userId_planId_key" ON "Subscription"("userId", "planId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Invoice_stripeInvoiceId_key" ON "Invoice"("stripeInvoiceId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wallet" ADD CONSTRAINT "Wallet_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WalletCryptos" ADD CONSTRAINT "WalletCryptos_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WalletCryptos" ADD CONSTRAINT "WalletCryptos_cryptoId_fkey" FOREIGN KEY ("cryptoId") REFERENCES "Crypto"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Trade" ADD CONSTRAINT "Trade_cryptoId_fkey" FOREIGN KEY ("cryptoId") REFERENCES "Crypto"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Trade" ADD CONSTRAINT "Trade_botId_fkey" FOREIGN KEY ("botId") REFERENCES "Bot"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApiKey" ADD CONSTRAINT "ApiKey_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bot" ADD CONSTRAINT "Bot_apiKeyId_fkey" FOREIGN KEY ("apiKeyId") REFERENCES "ApiKey"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bot" ADD CONSTRAINT "Bot_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bot" ADD CONSTRAINT "Bot_cryptoId_fkey" FOREIGN KEY ("cryptoId") REFERENCES "Crypto"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bot" ADD CONSTRAINT "Bot_strategieId_fkey" FOREIGN KEY ("strategieId") REFERENCES "Strategy"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_planId_fkey" FOREIGN KEY ("planId") REFERENCES "Plan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_subscriptionId_fkey" FOREIGN KEY ("subscriptionId") REFERENCES "Subscription"("id") ON DELETE CASCADE ON UPDATE CASCADE;
