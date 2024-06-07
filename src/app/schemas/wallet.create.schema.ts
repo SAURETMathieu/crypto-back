28d8f4c6f3b5cf8dddb48e757c8eb11ee18ee8ef
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-07 18:10:57 +0200

    feat(schema): add regex for wallet's addresses schemas

--- src/app/schemas/wallet.create.schema.ts
+++ src/app/schemas/wallet.create.schema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { exchanges, blockchains } from "../enum/enum";
+import { blockchains, exchanges } from "../enum/enum";
+import { getRegexForBlockchain } from "../data/addressesRegex";
 
 export const centralizedWalletCreateSchema = z.object({
   name: z
@@ -44,12 +45,22 @@ export const decentralizedWalletCreateSchema = z.object({
   }),
 
   address: z
-  .string({
-    required_error: "Wallet's address is required.",
-  })
-  .max(30, "Wallet must be at most 30 characters.")
-  .min(3, "Wallet must be at least 3 characters.")
-});
+      .string({
+        required_error: "Wallet's address is required.",
+      })
+      .max(44, "Wallet must be at most 44 characters.")
+      .min(26, "Wallet must be at least 26 characters.")
+})
+.refine(
+  (data) => {
+    const regex = getRegexForBlockchain(data.blockchain);
+    if (!regex) return false;
+    return regex.test(data.address);
+  },
+  {
+    message: `This address is unvalid for this blockchain.`,
+  }
+);;
 
 export type DecentralizedWallet = z.infer<typeof decentralizedWalletCreateSchema>;
 
