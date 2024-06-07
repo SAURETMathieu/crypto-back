086983b430aaa2b62dc8e822607fb87dcf300c20
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-07 21:21:36 +0200

    fix(core controller): remove useless try catch

--- src/app/controllers/core.controller.ts
+++ src/app/controllers/core.controller.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import prisma from "@/helpers/pg.prisma";
+import prisma from "../helpers/pg.prisma";
 
 export default class Controller {
-
-  static get table():string {
+  static get table(): string {
     throw new Error("Table name not defined");
   }
 
@@ -21,25 +20,24 @@ export default class Controller {
     response: Response,
     next: NextFunction
   ) {
-    try {
-      const { id } = request.params;
-      const row = await (prisma as any)[this.table].findUnique({ where: { id } });
-      if (!row) {
-        return next();
-      }
-      return response.status(200).json(row);
-    } catch (error) {
-      response.status(500).json({ error: "Internal Server Error" });
+    const { id } = request.params;
+    const row = await (prisma as any)[this.table].findUnique({
+      where: { id: parseInt(id, 10) },
+    });
+    if (!row) {
+      return next();
     }
+    return response.status(200).json(row);
   }
 
   static async create(request: Request, response: Response) {
-    try {
-      const row = await (prisma as any)[this.table].create({ data: request.body });
-      response.status(201).json(row);
-    } catch (error) {
-      response.status(500).json({ error: "Internal Server Error" });
-    }
+    const { body } = request;
+    body.userId = "clx0dzekq00018bidpe7al2t8";
+    body.type = "Centralized";
+    delete body.key;
+    console.log(body);
+    const row = await prisma.wallet.create({ data: body });
+    response.status(201).json(row);
   }
 
   static async update(
@@ -47,25 +45,21 @@ export default class Controller {
     response: Response,
     next: NextFunction
   ) {
-    try {
-      const { id } = request.params;
-      const dbData = await (prisma as any)[this.table].findUnique({
-        where: { id },
-      });
+    const { id } = request.params;
+    const dbData = await (prisma as any)[this.table].findUnique({
+      where: { id: parseInt(id, 10) },
+    });
 
-      if (!dbData) {
-        return next();
-      }
-
-      const data = { ...dbData, ...request.body };
-      const row = await (prisma as any)[this.table].update({
-        where: { id },
-        data,
-      });
-      return response.status(200).json(row);
-    } catch (error) {
-      response.status(500).json({ error: "Internal Server Error" });
+    if (!dbData) {
+      return next();
     }
+
+    const data = { ...dbData, ...request.body };
+    const row = await (prisma as any)[this.table].update({
+      where: { id },
+      data,
+    });
+    return response.status(200).json(row);
   }
 
   static async delete(
@@ -73,15 +67,13 @@ export default class Controller {
     response: Response,
     next: NextFunction
   ) {
-    try {
-      const { id } = request.params;
-      const deleted = await (prisma as any)[this.table].delete({ where: { id } });
-      if (!deleted) {
-        return next();
-      }
-      return response.status(204).json();
-    } catch (error) {
-      response.status(500).json({ error: "Internal Server Error" });
+    const { id } = request.params;
+    const deleted = await (prisma as any)[this.table].delete({
+      where: { id: parseInt(id, 10) },
+    });
+    if (!deleted) {
+      return next();
     }
+    return response.status(204).json();
   }
 }
