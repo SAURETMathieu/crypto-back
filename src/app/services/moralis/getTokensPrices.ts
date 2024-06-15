bdcd61ac627f95b8bee730fd586db8148450a7e4
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-06-15 19:09:35 +0200

    feat(services): function to get tokens prices

---
+++ src/app/services/moralis/getTokensPrices.ts
@@ -0,0 +1,33 @@
+import {
+  EvmTokenPriceItem,
+  EvmTokenPriceItemInput,
+} from "@moralisweb3/common-evm-utils";
+import Moralis from "moralis";
+
+//cost 100 per request => 400 per day max
+
+export async function getTokensPrices(
+  tokens: EvmTokenPriceItemInput[] | EvmTokenPriceItem[],
+  blockchain: string
+) {
+  try {
+    if (!Moralis.Core.isStarted) {
+      return null;
+    }
+
+    const response = await Moralis.EvmApi.token.getMultipleTokenPrices(
+      {
+        chain: "0x1",
+      },
+      {
+        tokens,
+      }
+    );
+
+    console.log(response.result);
+    return true;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
